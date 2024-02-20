21991356955737e1fd75016d455941143c28c239
Ethanosaure <ethandiasmarques.pro@gmail.com>
2024-02-20 16:58:11 +0100

    finishing css for success msg + responsive

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,39 @@
-const formElement = document.getElementById("form");
 const button = <HTMLButtonElement>document.getElementById("submitButton");
 const errorDiv = <HTMLDivElement>document.getElementById("errorElement");
 const emailInput = <HTMLInputElement>document.getElementById("emailInput");
+const form = <HTMLFormElement>document.getElementById("form");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const successMsg = <HTMLDivElement>document.getElementById("SuccessMessage");
+const newsLetter = <HTMLDivElement>document.getElementById("newsletter");
 
-button.addEventListener("click", (e) => {
-  const emailInputValue = (<HTMLInputElement>(
-    document.getElementById("emailInput")
-  )).value;
-  if (emailInputValue) {
-    console.log(emailInputValue);
-  } else {
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (emailInput.value) {
+      if (emailRegex.test(emailInput.value)) {
+        newsLetter.classList.add("hidden");
+        successMsg.classList.replace("hidden", "visible");
+      }
+    } else {
+      if (document.getElementById("errorText")) {
+        return;
+      }
+      const errorText = document.createElement("h3");
+      errorText.innerText = "valid email required";
+      errorText.setAttribute("id", "errorText");
+      emailInput.classList.add("bg-errorText");
+      errorDiv.appendChild(errorText);
+    }
+  });
+}
+
+if (emailInput) {
+  emailInput.addEventListener("click", (e) => {
+    e.preventDefault();
     if (document.getElementById("errorText")) {
+      document.getElementById("errorText")?.remove();
+      emailInput.classList.remove("bg-errorText");
       return;
     }
-    const errorText = document.createElement("h3");
-    errorText.setAttribute("id", "errorText");
-    errorText.classList.add("text-errorText");
-    errorText.innerHTML = "valid email required";
-    errorDiv.appendChild(errorText);
-    emailInput.classList.add("bg-errorInput");
-    emailInput.classList.replace("border-grey", "border-errorText");
-  }
-});
+  });
+}
